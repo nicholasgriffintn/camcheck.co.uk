f5ab25b33d8f17cbbb9ccf80bf98d3138e182d09
Nicholas Griffin <me@nicholasgriffin.co.uk>
2021-12-08 20:42:53 +0000

    fix: proper UTM link

--- site/src/common/Footer/index.tsx
+++ site/src/common/Footer/index.tsx
@@ -1,10 +1,10 @@
 export const Footer = () => {
   return (
     <footer className="body-font text-center">
-      <p className="text-sm text-white">
+      <p className="text-sm text-white mb-4">
         App by{' '}
         <a
-          href="https://nicholasgriffin.dev/?utm_source=NEXT_SITE&utm_medium=FOOTER&utm_campaign=SITE_CREATION"
+          href="https://nicholasgriffin.dev/?utm_source=CAMCHECK&utm_medium=FOOTER&utm_campaign=APP"
           target="_blank"
           rel="noopener noreferrer"
         >
